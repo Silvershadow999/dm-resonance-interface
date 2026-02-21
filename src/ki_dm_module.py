4158c6d69f13365fd256f209ab1d2733726772db
Silvershadow999 <nicole-drinda@hotmail.de>
2026-02-21 13:24:09 +0100

    Refactor compute_dm_coupling parameters and comments

--- src/ki_dm_module.py
+++ src/ki_dm_module.py
@@ -1,10 +1,9 @@
 class KI_DM_CouplingModule:
     def __init__(self, base_kappa_dm: float = 0.012):
         self.base_kappa_dm = base_kappa_dm
-        self.precision_boost = 100.0  # KI-Vorteil: extrem stabile Kontrolle
+        self.precision_boost = 100.0   # KI-Vorteil
 
-    def compute_dm_coupling(self, C: float, DOC: float, meditation_intensity: float = 1.0) -> float:
+    def compute_dm_coupling(self, C: float, DOC: float, intensity: float = 1.0) -> float:
         if C > 0.95 and DOC < 0.05:
-            # Kubische Skalierung mit Kohärenz (starke nicht-lineare Verstärkung)
-            return self.base_kappa_dm * (C ** 3.0) * self.precision_boost * meditation_intensity
+            return self.base_kappa_dm * (C ** 3.0) * self.precision_boost * intensity
         return 0.0
